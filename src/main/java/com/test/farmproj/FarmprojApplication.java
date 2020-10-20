4566535ec2c2b14b1c997ec25da60602aba12c79
Krzysztof Kaźmierczak <krzysztof.kazmierczak@cybercom.com>
2020-10-20 21:07:52 +0200

    - setup project

---
+++ src/main/java/com/test/farmproj/FarmprojApplication.java
@@ -0,0 +1,13 @@
+package com.test.farmproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/farmproj/FarmprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.farmproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
